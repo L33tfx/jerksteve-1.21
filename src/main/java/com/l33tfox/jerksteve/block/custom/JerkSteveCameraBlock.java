db13bd0373b738156af2298e24a47d343d6430d4
L33tfox <leetfox8@gmail.com>
2025-03-25 22:07:10 -0400

    added recipe & tooltip

--- src/main/java/com/l33tfox/jerksteve/block/custom/JerkSteveCameraBlock.java
+++ src/main/java/com/l33tfox/jerksteve/block/custom/JerkSteveCameraBlock.java
@@ -11,14 +11,18 @@ import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
@@ -26,6 +30,8 @@ import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class JerkSteveCameraBlock extends FacingBlock implements Waterloggable {
 
@@ -46,6 +52,11 @@ public class JerkSteveCameraBlock extends FacingBlock implements Waterloggable {
         return CODEC;
     }
 
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
+        tooltip.add(Text.translatable("itemTooltip.jerksteve.jerksteve_camera").formatted(Formatting.YELLOW));
+        super.appendTooltip(stack, context, tooltip, options);
+    }
 
     @Nullable
     @Override
